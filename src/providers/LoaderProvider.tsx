bfa1cb02286f4244b4c77d25b19001ca63d94b48
VeiaG <romapalamar.veiag@gmail.com>
2025-12-18 01:48:16 +0200

    feat: update gradient color in LoaderProvider for improved visual consistency

--- src/providers/LoaderProvider.tsx
+++ src/providers/LoaderProvider.tsx
@@ -2,14 +2,21 @@
 
 import { AppProgressProvider as ProgressProvider } from '@bprogress/next'
 
+const gradientColor = 'linear-gradient(90deg, #D2691E 0%, #FF8C42 100%)'
 const LoaderProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <ProgressProvider
       height="4px"
-      color="linear-gradient(90deg, #FF29C3 0%, #174AFF 100%)"
+      color={gradientColor}
       options={{ showSpinner: false }}
       shallowRouting
     >
+      {/* <div
+        className="fixed z-[9999] top-0 left-0 w-screen h-1"
+        style={{
+          background: gradientColor,
+        }}
+      ></div> */}
       {children}
     </ProgressProvider>
   )
